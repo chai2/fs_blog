6f13fd377cab10e9ec5f31e1768d5f191ec361eb
Chaitanya Malla <chaitulittle@gmail.com>
2012-03-03 17:44:54 -0500

    more changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,7 @@ set :scm, :git
 set :deploy_to, "/var/www/apps/fashionsprout_blog/"
 
 set :deploy_via, :remote_cache
-set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
+set :copy_include, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
 
-server "staging.fashionsprout.com", :app
+server "blog.fashionsprout.com", :app
 
-task :symlink_config, :except => {:no_release => true, :no_symlink => true} do
-  run "ln -nsf #{shared_path}/wp-config.php #{current_release}"
-  # run "cd #{release_path} && export PATH=/usr/local/ruby-enterprise/bin:$PATH && bundle install vendor/bundle"
-  # run "cd #{release_path}; #{try_sudo} /usr/local/ruby-enterprise/bin/bundle install"
-end
